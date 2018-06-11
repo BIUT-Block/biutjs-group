a344feaeef1ff4876de0b0137b49eb9d571a8cbe
Zu <zuweihan1992@gmail.com>
2018-06-11 21:35:35 +0200

    npm initialized, test file basic structure implemented

--- test/test.js
+++ test/test.js
@@ -2,7 +2,73 @@ const SecjsHash = require('../src/index')
 const expect = require('chai').expect
 
 describe('SecjsGroup', () => {
-  describe('', () => {
+  describe('generateGroupId() function test', () => {
+    it('generated group ID is within the defined range', () => {
+      expect(() => {}).to.not.throw()
+    })
+  })
+  
+  describe('generateGroupIds(peerAddrList) function test', () => {
+    it('', () => {
+      expect(() => {}).to.not.throw()
+    })
+  })
+  
+  describe('getGroupId(accAddr) function test', () => {
+    it('', () => {
+      expect(() => {}).to.not.throw()
+    })
+  })
+  
+  describe('getGeneratedPeerGroupId() function test', () => {
+    it('', () => {
+      expect(() => {}).to.not.throw()
+    })
+  })
+  
+  describe('getGroupIdDht() function test', () => {
+    it('', () => {
+      expect(() => {}).to.not.throw()
+    })
+  })
+  
+  describe('getGroupIdStatisticsDht() function test', () => {
+    it('', () => {
+      expect(() => {}).to.not.throw()
+    })
+  })
+  
+  describe('setGroupId(accAddr, groupId) function test', () => {
+    it('', () => {
+      expect(() => {}).to.not.throw()
+    })
+  })
+  
+  describe('updateStatisticsDht(peerAccGroupIdDht) function test', () => {
+    it('', () => {
+      expect(() => {}).to.not.throw()
+    })
+  })
+  
+  describe('setGroupIdDht() function test', () => {
+    it('', () => {
+      expect(() => {}).to.not.throw()
+    })
+  })
+  
+  describe('storeGroupIdTableToFile(file, content) function test', () => {
+    it('', () => {
+      expect(() => {}).to.not.throw()
+    })
+  })
+  
+  describe('_accAddrValidate(accAddr) function test', () => {
+    it('', () => {
+      expect(() => {}).to.not.throw()
+    })
+  })
+  
+  describe('_groupIdValidate(groupId) function test', () => {
     it('', () => {
       expect(() => {}).to.not.throw()
     })
