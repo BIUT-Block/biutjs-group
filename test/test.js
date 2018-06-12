82ab219f254efeb391da43c3f25465cae57a1043
Zu <zuweihan1992@gmail.com>
2018-06-12 20:29:19 +0200

    removed useless functions

--- test/test.js
+++ test/test.js
@@ -1,73 +1,58 @@
-const SecjsHash = require('../src/index')
+const SecjsGroup = require('../src/index')
 const expect = require('chai').expect
 
 describe('SecjsGroup', () => {
+  let config = {'accAddrLength' : 32}
+  const secGroup = new SecjsGroup(config)
+
   describe('generateGroupId() function test', () => {
     it('generated group ID is within the defined range', () => {
       expect(() => {}).to.not.throw()
     })
   })
-  
+
   describe('generateGroupIds(peerAddrList) function test', () => {
     it('', () => {
       expect(() => {}).to.not.throw()
     })
   })
-  
+
   describe('getGroupId(accAddr) function test', () => {
     it('', () => {
       expect(() => {}).to.not.throw()
     })
   })
-  
-  describe('getGeneratedPeerGroupId() function test', () => {
-    it('', () => {
-      expect(() => {}).to.not.throw()
-    })
-  })
-  
-  describe('getGroupIdDht() function test', () => {
-    it('', () => {
-      expect(() => {}).to.not.throw()
-    })
-  })
-  
-  describe('getGroupIdStatisticsDht() function test', () => {
-    it('', () => {
-      expect(() => {}).to.not.throw()
-    })
-  })
-  
+
   describe('setGroupId(accAddr, groupId) function test', () => {
     it('', () => {
       expect(() => {}).to.not.throw()
     })
   })
-  
+
   describe('updateStatisticsDht(peerAccGroupIdDht) function test', () => {
     it('', () => {
       expect(() => {}).to.not.throw()
     })
   })
-  
+
   describe('setGroupIdDht() function test', () => {
     it('', () => {
       expect(() => {}).to.not.throw()
     })
   })
-  
+
   describe('storeGroupIdTableToFile(file, content) function test', () => {
     it('', () => {
       expect(() => {}).to.not.throw()
     })
   })
-  
+
   describe('_accAddrValidate(accAddr) function test', () => {
     it('', () => {
       expect(() => {}).to.not.throw()
     })
   })
-  
+
   describe('_groupIdValidate(groupId) function test', () => {
     it('', () => {
       expect(() => {}).to.not.throw()
